36845d413121f484872743caf1ae9aa4e698c962
Natalie Blumen <natalie.blumen@gmail.com>
2025-06-20 13:02:11 -0400

    jun20

--- dist/route/taskRoutes.js
+++ dist/route/taskRoutes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 import taskController from "../controllers/task.js";
 router.get("/", taskController.getIndex);
 router.post("/addTask", taskController.addTask);
-router.put("/completeTask/:id", taskController.completeTask);
-router.delete("/deleteTask/:id", taskController.deleteTask);
+router.post("/completeTask/:id", taskController.completeTask);
+router.post("/deleteTask/:id", taskController.deleteTask);
 export default router;
