450c693cf7f612725e81f39498d5c207099fb653
Natalie Blumen <natalie.blumen@gmail.com>
2025-06-23 13:36:40 -0400

    jun23

--- dist/controllers/todoistTask.js
+++ dist/controllers/todoistTask.js
@@ -15,5 +15,49 @@ const todoistTaskController = {
             res.status(500).send("Error fetching tasks from the todoist");
         }
     },
+    addTask: async (req, res) => {
+        try {
+            const response = await fetch("https://api.todoist.com/rest/v2/tasks", {
+                method: "POST",
+                headers: {
+                    "Authorization": `Bearer ${process.env.TODOIST_API_TOKEN}`
+                },
+                body: JSON.stringify({
+                    content: req.body.content
+                })
+            });
+            res.redirect("/todoist");
+        }
+        catch (err) {
+            console.error(err);
+            res.status(500).send("Error adding a task to the todoist");
+        }
+    },
+    completeTask: async (req, res) => {
+        try {
+            const response = await fetch(`https://api.todoist.com/rest/v2/tasks/${taskID}/close`, {
+                method: "POST",
+                headers: { "Authorization": `Bearer ${process.env.TODOIST_API_TOKEN}` }
+            });
+            res.redirect("/todoist");
+        }
+        catch (err) {
+            console.error(err);
+            res.status(500).send("Error completing a task on the todoist");
+        }
+    },
+    deleteTask: async (req, res) => {
+        try {
+            const response = await fetch(`https://api.todoist.com/rest/v2/tasks/${taskID}`, {
+                method: "DELETE",
+                headers: { "Authorization": `Bears ${process.env.TODOIST_API_TOKEN}` }
+            });
+            res.redirect("/todoist");
+        }
+        catch (err) {
+            console.error(err);
+            res.status(500).send("Error deleting a task on the todoist");
+        }
+    }
 };
 export default todoistTaskController;
